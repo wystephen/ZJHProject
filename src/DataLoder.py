df1332da3a822e424bb82d553c88a4f06957e48b
steve <551619855@qq.com>
2018-02-04 23:54:46 +0800

    s

--- src/DataLoder.py
+++ src/DataLoder.py
@@ -23,5 +23,35 @@
          佛祖保佑       永无BUG 
 '''
 
-import torch
 
+import numpy as np
+import scipy as sp
+
+import matplotlib.pyplot as plt
+
+class DataLoader:
+    def __init__(self, file_name='/home/steve/Data/ZJHData/source_data.csv'):
+        self.file_name = file_name
+        self.data = np.loadtxt(self.file_name,delimiter=',')
+        self.normlized_data = self.data-self.data.mean(axis=0)
+        self.normlized_data /= self.normlized_data.std(axis=0)
+
+if __name__ == '__main__':
+    dl = DataLoader()
+
+    plt.figure()
+    plt.plot(dl.data[:,0])
+
+    plt.figure()
+    for i in range(1,dl.data.shape[1]):
+        plt.plot(dl.data[:,i],label=str(i))
+    plt.legend()
+    plt.grid()
+    plt.figure()
+    for i in range(1,dl.data.shape[1]):
+        plt.plot(dl.normlized_data[:,i],label=str(i))
+    plt.legend()
+    plt.grid()
+
+
+    plt.show()
